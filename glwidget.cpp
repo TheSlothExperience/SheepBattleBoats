86c5d5f8b9e82e20481599e23ee0ea77a464a010
Sebastian Mendez <sebas.chinoir@gmail.com>
2014-04-25 21:12:46 +0200

    Now displays a colored cube.
    The drawCube function accepts different levels of tesselation.

--- glwidget.cpp
+++ glwidget.cpp
@@ -1,7 +1,9 @@
 #include "glwidget.h"
+
 #include <QtGui>
 #include <QtOpenGL>
 #include "GL/glu.h"
+#include <math.h>
 
 GLWidget::GLWidget(QWidget *parent)
     : QGLWidget(QGLFormat(QGL::SampleBuffers), parent)
@@ -31,23 +33,93 @@ void GLWidget::initializeGL()
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
     glEnable(GL_MULTISAMPLE);
+    //glPolygonMode(GL_FRONT, GL_LINE);
+    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+    glEnable(GL_COLOR_MATERIAL);
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);    
-    static GLfloat lightPosition[4] = { 0.5, 5.0, 7.0, 1.0 };
+    static GLfloat lightPosition[4] = {0.5, 0.0, 2.0, 1.0};
     glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
+}
+
+void drawCube(int tesselation = 0) {
+    int squares = pow(2, tesselation);
+    double step = 1.0/squares;
+
+    //front
+    glColor3f(0.0,0.0,1.0);
+    for (int y = 0; y < squares; y++) {
+	glBegin(GL_TRIANGLE_STRIP);
+	for (int x = 0; x <= squares; x++) {
+	    glVertex3f(-0.5 + x*step, 0.5 - y*step, 0.5);
+	    glVertex3f(-0.5 + x*step, 0.5 - (y+1)*step, 0.5);
+	}
+	glEnd();
+    }
+
+    //right
+    glColor3f(1.0, 0.0, 0.0);
+    for (int y = 0; y < squares; y++) {
+	glBegin(GL_TRIANGLE_STRIP);
+	for (int x = 0; x <= squares; x++) {
+	    glVertex3f(0.5, 0.5 - y*step, 0.5 - x*step);
+	    glVertex3f(0.5, 0.5 - (y+1)*step, 0.5 - x*step);
+	}
+	glEnd();
+    }
+
+    //top
+    glColor3f(0.0, 1.0, 0.0);
+    for (int y = 0; y < squares; y++) {
+	glBegin(GL_TRIANGLE_STRIP);
+	for (int x = 0; x <= squares; x++) {
+	    glVertex3f(-0.5 + x*step, 0.5, -0.5 + y*step);
+	    glVertex3f(-0.5 + x*step, 0.5, -0.5 + (y+1)*step);
+	}
+	glEnd();
+    }
+
+    //back
+    glColor3f(1.0, 1.0, 0.0);
+    for (int y = 0; y < squares; y++) {
+	glBegin(GL_TRIANGLE_STRIP);
+	for (int x = 0; x <= squares; x++) {
+	    glVertex3f(0.5 - x*step, 0.5 - y*step, -0.5);
+	    glVertex3f(0.5 - x*step, 0.5 - (y+1)*step, -0.5);
+	}
+	glEnd();
+    }
+
+    //left
+    glColor3f(0.0, 1.0, 1.0);
+    for (int y = 0; y < squares; y++) {
+	glBegin(GL_TRIANGLE_STRIP);
+	for (int x = 0; x <= squares; x++) {
+	    glVertex3f(-0.5, 0.5 - y*step, -0.5 + x*step);
+	    glVertex3f(-0.5, 0.5 - (y+1)*step, -0.5 + x*step);
+	}
+	glEnd();
+    }
+
+    //bottom
+    glColor3f(1.0, 0.0, 1.0);
+    for (int y = 0; y < squares; y++) {
+	glBegin(GL_TRIANGLE_STRIP);
+	for (int x = 0; x <= squares; x++) {
+	    glVertex3f(0.5 - x*step, -0.5, -0.5 + y*step);
+	    glVertex3f(0.5 - x*step, -0.5, -0.5 + (y+1)*step);
+	}
+	glEnd();
+    }
 }
 
 void GLWidget::paintGL()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glPushMatrix();
-    static GLfloat color[4] = {1.0,1.0,1.0,1.0};
-    glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, color);
-    glBegin(GL_QUADS);
-    glVertex3f(1.0, 1.0, 0.0);
-    glVertex3f(-1.0, 1.0, 0.0);
-    glVertex3f(-1.0, -1.0, 0.0);
-    glVertex3f(1.0, -1.0, 0.0);
-    glEnd();
+    glTranslatef(0.0f, 0.0f, -3.0f);
+    glRotatef(45, 0.0, 1.0, 0.0);
+    glRotatef(-20, 1.0, 0.0, 0.0);
+    drawCube(3);
     glPopMatrix();
 }
 
