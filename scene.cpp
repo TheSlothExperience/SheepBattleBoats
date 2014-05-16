70c1dbd948d9f3251a27a27740cd58a09a79ed5e
Sebastian Mendez <sebas.chinoir@gmail.com>
2014-05-17 00:58:58 +0200

    Added QAbstractItemModel functions in Scene.

--- scene.cpp
+++ scene.cpp
@@ -9,19 +9,108 @@
 #include <GL/gl.h>
 #include <iostream>
 
-Scene::Scene() {
+Scene::Scene(QObject *parent)
+    : QAbstractItemModel(parent)
+{
     this->modelViewMatrixStack.push(QMatrix4x4());
 }
-Scene::Scene(GLuint mvLoc, GLuint normalLoc) {
+Scene::Scene(GLuint mvLoc, GLuint normalLoc, QObject *parent)
+    : QAbstractItemModel(parent)
+{
     this->modelViewMatLocation = mvLoc;
     this->normalMatLocation = normalLoc;
     this->modelViewMatrixStack.push(QMatrix4x4());
 
     Cube *cube = new Cube();
-    this->sceneGraph = new SceneGraph();
-    this->sceneGraph->add(cube);
+    this->rootNode = new SceneGraph();
+    this->rootNode->add(cube);
     lightPosition = QVector4D(0.5, 0.0, 2.0, 1.0);
 }
+
+
+QModelIndex Scene::index(int row, int column, const QModelIndex &parent) const {
+    if(!hasIndex(row, column, parent)) {
+	return QModelIndex();
+    }
+
+    SceneGraph *parentNode;
+
+    if(!parent.isValid()) {
+	parentNode = rootNode;
+    } else {
+	parentNode = static_cast<SceneGraph*>(parent.internalPointer());
+    }
+
+    SceneGraph *childNode = parentNode->child(row);
+    if(childNode) {
+	return createIndex(row, column, childNode);
+    } else {
+	return QModelIndex();
+    }
+}
+
+QModelIndex Scene::parent(const QModelIndex &index) const {
+    if(!index.isValid()) {
+	return QModelIndex();
+    }
+
+    SceneGraph *childNode = static_cast<SceneGraph*>(index.internalPointer());
+    SceneGraph *parentNode = childNode->parent();
+
+    if(parentNode == rootNode) {
+	return QModelIndex();
+    } else {
+	return createIndex(parentNode->row(), 0, parentNode);
+    }
+}
+
+int Scene::rowCount(const QModelIndex &parent) const {
+    SceneGraph *parentNode;
+    if(parent.column() > 0) {
+	return 0;
+    }
+
+    if(!parent.isValid()) {
+	parentNode = rootNode;
+    } else {
+	parentNode = static_cast<SceneGraph*>(parent.internalPointer());
+    }
+    return parentNode->childCount();
+}
+
+int  Scene::columnCount(const QModelIndex &parent) const {
+    if(parent.isValid()) {
+	return static_cast<SceneGraph*>(parent.internalPointer())->columnCount();
+    } else {
+	return rootNode->columnCount();
+    }
+}
+
+QVariant Scene::data(const QModelIndex &index, int role) const {
+    if(!index.isValid()) {
+	return QVariant();
+    }
+    if(role != Qt::DisplayRole) {
+	return QVariant();
+    }
+
+    SceneGraph *node = static_cast<SceneGraph*>(index.internalPointer());
+
+    return node->data(index.column());
+}
+
+Qt::ItemFlags Scene::flags(const QModelIndex &index) const {
+    if(!index.isValid()) {
+	return 0;
+    } else {
+	return QAbstractItemModel::flags(index);
+    }
+}
+
+QVariant Scene::headerData(int section, Qt::Orientation orientation, int role) const {
+    return QVariant("Scene Model");
+}
+
 
 void Scene::setLightLocation(GLuint lightPositionLocation) {
     this->lightPositionLocation = lightPositionLocation;
@@ -35,6 +124,6 @@ void Scene::draw(QMatrix4x4 cameraMatrix) {
     GLfloat lightDirArray[3] = {lightDir.x(), lightDir.y(), lightDir.z()};
     glUniform3fv(lightPositionLocation, 1, lightDirArray);
     
-    this->sceneGraph->draw(modelViewMatrixStack, modelViewMatLocation, normalMatLocation);
+    this->rootNode->draw(modelViewMatrixStack, modelViewMatLocation, normalMatLocation);
     modelViewMatrixStack.pop();
 }
--- scene.h
+++ scene.h
@@ -5,11 +5,16 @@
 
 #include <stack>
 #include <QMatrix4x4>
+#include <QAbstractItemModel>
+#include <QModelIndex>
+#include <QVariant>
 
-class Scene
+class Scene : public QAbstractItemModel
 {
+    Q_OBJECT
+    
 private:
-    SceneGraph *sceneGraph;
+    SceneGraph *rootNode;
 
     
     GLuint modelViewMatLocation;
@@ -20,8 +25,19 @@ private:
     std::stack<QMatrix4x4> modelViewMatrixStack;
 
 public:
-    Scene();
-    Scene(GLuint mvLoc, GLuint normalLoc);
+    Scene(QObject *parent = 0);
+    Scene(GLuint mvLoc, GLuint normalLoc, QObject *parent = 0);
+    
+    QVariant data(const QModelIndex &index, int role) const;
+    Qt::ItemFlags flags(const QModelIndex &index) const;
+    QVariant headerData(int section, Qt::Orientation orientation,
+                        int role = Qt::DisplayRole) const;
+    QModelIndex index(int row, int column,
+                      const QModelIndex &parent = QModelIndex()) const;
+    QModelIndex parent(const QModelIndex &index) const;
+    int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const;
+    
     void setLightLocation(GLuint lightPositionLocation);
     void draw(QMatrix4x4 cameraMatrix);
 };
