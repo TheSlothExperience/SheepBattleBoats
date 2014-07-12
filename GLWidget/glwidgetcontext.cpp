4f9573c06aa13d55d684d8f430cdc92a424478d5
Sebastian Mendez <sebas.chinoir@gmail.com>
2014-07-12 16:24:18 +0200

    Fix for shader locations.

--- GLWidget/glwidgetcontext.cpp
+++ GLWidget/glwidgetcontext.cpp
@@ -26,7 +26,7 @@ GLWidgetContext::~GLWidgetContext()
 void GLWidgetContext::initializeGL()
 {
 	glEnable(GL_TEXTURE_3D);
-    loadShaders(":/shaders/shaders/xtoon.vert", ":/shaders/shaders/xtoon.frag", vphong, fphong, shaderProgram);
+    loadShaders(":/shaders/xtoon.vert", ":/shaders/xtoon.frag", vphong, fphong, shaderProgram);
 
 	shaderProgram->bind();
 
@@ -38,8 +38,8 @@ void GLWidgetContext::initializeGL()
 
 	shaderProgram->release();
 
-    loadShaders(":/shaders/shaders/identity.vert", ":/shaders/shaders/canvas.frag", vcanvas, fcanvas, canvasProgram);
-    loadShaders(":/shaders/shaders/viewpoint.vert", ":/shaders/shaders/viewpoint.frag", vquadview, fquadview, quadviewProgram);
+    loadShaders(":/shaders/identity.vert", ":/shaders/canvas.frag", vcanvas, fcanvas, canvasProgram);
+    loadShaders(":/shaders/viewpoint.vert", ":/shaders/viewpoint.frag", vquadview, fquadview, quadviewProgram);
 
 }
 
