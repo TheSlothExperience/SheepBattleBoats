45917c9316d35b542efcfc4e2442518eb4cc3dfb
Sebastian Mendez <sebas.chinoir@gmail.com>
2014-07-15 22:14:32 +0200

    Some Emacs local dir variables (you can ignore this).

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((nil . ((eval . (setq default-directory (locate-dominating-file
+										  buffer-file-name ".dir-locals.el")
+					   ))))
+ (c++-mode . ((compile-command . "cd ../build/debug && qmake ../../SheepBattleBoats/SheepBattleBoats.pro -r -spec linux-g++-64 && make -j4")))
+ (c-mode . ((compile-command . "cd ../build/debug && qmake ../../SheepBattleBoats/SheepBattleBoats.pro -r -spec linux-g++-64 && make -j4"))))
